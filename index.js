4992f5b7cc7ebf9e40c2b19047e7912499e65edf
ThomasMab <71639024+ThomasMab@users.noreply.github.com>
2020-10-28 15:16:07 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 // récupère les infos de connection du .env
 const dotenv = require('dotenv');
 dotenv.config();
-//facilite la création du serveur
-const express = require('express');
 const router = require('./app/router');
 //log des requêtes dans la console
 const morgan = require('morgan');
 //pour les CORS
 const cors = require('cors');
 
+var fs = require('fs'),
+  https = require('https'),
+  express = require('express');
+  const app = express();
 
+ 
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}, app).listen(8081);
+ 
+app.get('/', function(req, res) {
+  res.header('Content-type', 'text/html');
+  return res.end('<h1>HTTPS WORKS!</h1>');
+});
 
-const PORT = process.env.PORT || 5050;
-const app = express();
 // permet de débloquer les CORS
 app.use(cors());
-
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 app.use(router);
 
-
-// Lance le serveur sur un port
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT} ...`);
-});
